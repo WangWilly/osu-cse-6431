68a36680c6411ce989f378e295f5e787d929f3f7
WangWilly <s89667654@gmail.com>
2024-09-04 15:00:04 -0400

    refactor: 💡 remove unnecessary functions

--- hw1/CondObj.java
+++ hw1/CondObj.java
@@ -1,17 +1,5 @@
 package hw1;
 
 public class CondObj {
-    private boolean condition;
-
-    public CondObj() {
-        this.condition = false;
-    }
-
-    public synchronized void setCondition(boolean condition) {
-        this.condition = condition;
-    }
-
-    public synchronized boolean getCondition() {
-        return condition;
-    }
+    public CondObj() {}
 }
